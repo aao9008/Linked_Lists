39e3af5ab0e6a92019ff2062b5b7eae627129009
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-08-19 17:33:00 -0400

    Add Node class and its initialization method

---
+++ node.rb
@@ -0,0 +1,10 @@
+class Node
+  # Initialize new data struture, use null values if no parameters are passed
+  def  initialize( data = null, next_node = null)
+    # node_data variable will hold single element of data
+    @node_data = data
+    # next_node variable holds link/pointer to next node in the list.
+    @next_node = next_node
+  end 
+  attr_accessor :node_data, :next_node
+end
