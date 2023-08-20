702f359943231d9cd8351f9a26c7dcd76bf60530
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-08-19 23:11:49 -0400

    Add #next_node method to the Node class

--- node.rb
+++ node.rb
@@ -4,12 +4,17 @@ class Node
     # node_data variable will hold single element of data
     @node_data = data
     # next_node variable holds link/pointer to next node in the list.
-    @next_node = next_node
+    @pointer = next_node
   end 
-  attr_accessor :node_data, :next_node
+  attr_accessor :node_data, :pointer
 
-  # This method will chande the node data value to the passed parameter 
+  # This method will change the node data value to the passed parameter.
   def value(data = nil)
     node_data = data
-  end 
+  end
+
+  # This method will change the pointer varaible value to the passed parameter.
+  def next_node(node)
+    pointer = node
+  end
 end
